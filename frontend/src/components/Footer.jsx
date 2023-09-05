ea4f2ea14ea338b36b4941159a06726cf9d8418c
Mahesh Abhale <abhale.mahesh@iitgn.ac.in>
2023-09-06 01:13:48 +0530

    Basic frontend done

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+ 
+const Footer = () => {
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <footer>
+            <Container>
+                <Row>
+                    <Col>
+                        <p className='text-center py-3'>
+                            Shop &copy; {currentYear}
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
+ 
+export default Footer
+ 
