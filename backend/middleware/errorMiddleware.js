d49934e1fdebce7eed5d5245fdefa831c8f87dc6
Mahesh Abhale <abhale.mahesh@iitgn.ac.in>
2023-09-16 16:19:57 +0530

    custom error middleware done

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,22 +1,22 @@
-// const notFound = (req, res, next) => {
-//     const error = new Error(`Not Found - ${req.originalUrl}`);
-//     res.status(404);
-//     next(error);
-// };
+const notFound = (req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+};
 
-// const errorHandler = (err, req, res, next) => {
-//     let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-//     let message = err.message;
+const errorHandler = (err, req, res, next) => {
+    let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    let message = err.message;
 
-//     if (err.name === 'CastError' && err.kind === 'ObjectId') {
-//         message = `Resource not found`;
-//         statusCode = 404;
-//     }
+    if (err.name === 'CastError' && err.kind === 'ObjectId') {
+        message = `Resource not found`;
+        statusCode = 404;
+    }
 
-//     res.status(statusCode).json({
-//         message,
-//         stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
-//     });
-// };
+    res.status(statusCode).json({
+        message,
+        stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
+    });
+};
 
-// export { notFound, errorHandler };
+export { notFound, errorHandler };
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 5000;
 
 connectDB(); // Connect to MongoDB
@@ -14,6 +15,9 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log("Server started at http://localhost:5000");
